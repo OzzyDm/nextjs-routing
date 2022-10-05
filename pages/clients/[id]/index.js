ea6ffddca10517528164bfd183e2bec43d1595a1
unknown <oguzhandemiraslan@gmail.com>
2022-10-05 00:09:32 -0400

    navigating dynamically and programmatically added

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientproject]",
+      query: { id: "cr7", clientproject: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
